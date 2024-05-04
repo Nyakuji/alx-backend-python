5f1db42c6abe65a709da3b7f0f1d59fbc1e66623
philip Opiyo <opiyophilipo@gmail.com>
2024-05-04 19:17:56 +0300

    feat: Add make_multiplier function to create a multiplier function with a given float multiplier

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Complex types - functions """
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Takes a float multiplier as argument and returns a function
+    that multiplies a float by multiplier.
+
+    Parameters:
+        multiplier (float): The float multiplier.
+
+    Returns:
+        Callable[[float], float]: A function that multiplies a float
+        by multiplier.
+    """
+    def multiply(n: float) -> float:
+        return n * multiplier
+    return multiply
