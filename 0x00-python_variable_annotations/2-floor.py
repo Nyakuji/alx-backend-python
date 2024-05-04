b3e964305472ee50eb82e6888e6863acc2e268f3
philip Opiyo <opiyophilipo@gmail.com>
2024-05-04 18:31:48 +0300

    feat: Add floor function to round down a float to the nearest integer

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Floor a float and return the integer value"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Floor a float and return the integer value"""
+    return math.floor(n)
