9b11222f5503d0f75dc4ab1fc11ea94aab568926
philip Opiyo <opiyophilipo@gmail.com>
2024-05-05 08:35:12 +0300

    feat: Add type checking to zoom_array function

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Type Checking """
+from typing import Tuple, List, Any, Union
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """ Type Checking """
+    zoomed_in: List = [
+        item for item in lst for i in range(factor)]
+    return zoomed_in
