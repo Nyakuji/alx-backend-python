6155b356400ef33b249b0be81b00ce521bc0ce3e
philip Opiyo <opiyophilipo@gmail.com>
2024-05-06 10:49:11 +0300

    feat: Implement wait_n function to asynchronously spawn tasks and return delays in ascending order

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Let's execute multiple coroutines at the same time with async"""
+
+import asyncio
+import random
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Asynchronously spawns wait_random tasks and
+    returns delays in ascending order.
+
+    Parameters:
+        n (int): Number of tasks to spawn.
+        max_delay (int): Maximum delay value for each task.
+
+    Returns:
+        List[float]: List of delays (float values) in ascending order.
+    """
+    delays = [wait_random(max_delay) for _ in range(n)]
+    return [await delay for delay in asyncio.as_completed(delays)]
