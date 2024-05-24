fbdba3c4b38860543c8d1a44d9a25d46fd9fe383
philip Opiyo <opiyophilipo@gmail.com>
2024-05-24 19:24:50 +0300

    refactor: Add PropertyMock to test_client.py for mocking org property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Unittests for the client module."""
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -20,3 +20,12 @@ class TestGithubOrgClient(unittest.TestCase):
         test_class.org()
         mock_get_json.assert_called_once_with(
             f"https://api.github.com/orgs/{org}")
+
+    @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org):
+        """Test that the result of _public_repos_url is the expected one."""
+        mock_org.return_value = {
+            'repos_url': 'https://api.github.com/orgs/google/repos'}
+        test_class = GithubOrgClient("google")
+        self.assertEqual(test_class._public_repos_url,
+                         "https://api.github.com/orgs/google/repos")
