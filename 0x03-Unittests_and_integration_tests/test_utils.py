996b117c45c73aac42b0d41fc25025c8579e74f8
philip Opiyo <opiyophilipo@gmail.com>
2024-05-24 10:35:03 +0300

    feat: Add unit tests and integration tests for 0x03-Unittests_and_integration_tests

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""testing utils module"""
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Class for testing access_nested_map"""
+    @parameterized.expand([
+        ({'a': 1}, ('a',), 1),
+        ({'a': {'b': 2}}, ('a', 'b'), 2),
+        ({'a': {'b': 2}}, ('a', 'b'), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Test that the function returns what it is supposed to"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
