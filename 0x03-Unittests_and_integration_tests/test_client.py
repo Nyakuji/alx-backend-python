c44413158538b58a144bf3d6338a555fd2fcb930
philip Opiyo <opiyophilipo@gmail.com>
2024-05-24 20:16:16 +0300

    refactor: Add parameterized unit test for has_license method in GithubOrgClient

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -47,3 +47,13 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_get_json.assert_called_once_with(
                 "https://api.github.com/orgs/google/repos")
             mock_public_repos_url.assert_called_once()
+
+    @parameterized.expand([
+        ({'license': {'key': 'my_license'}}, 'my_license', True),
+        ({'license': {'key': 'other_license'}}, 'my_license', False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """Test that the result of has_license is the expected one."""
+        test_class = GithubOrgClient("google")
+        result = test_class.has_license(repo, license_key)
+        self.assertEqual(result, expected)
